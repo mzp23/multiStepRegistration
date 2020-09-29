94e77599c84d5434f948ce7ea3732712b8065703
Vitalii <vitalii_sliusar@epam.com>
2020-09-29 14:41:15 +0300

    took styled components to their own files

---
+++ src/components/registration/select/Select.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const CustomSelect = styled.select`
+padding: 10px 5px;
+margin-bottom: 25px;
+border: 1px solid #e5e5ea;
+border-radius: 5px;
+outline: none;
+`;
