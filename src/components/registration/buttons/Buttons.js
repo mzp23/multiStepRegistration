a46bc791e74fd1292727d822333e312fd91059d1
Vitalii <vitalii_sliusar@epam.com>
2020-09-29 14:32:02 +0300

    took condition of render PrevButton to parent component

--- src/components/registration/buttons/Buttons.js
+++ src/components/registration/buttons/Buttons.js
@@ -2,12 +2,16 @@ import React from 'react';
 import NextButton from './NextButton';
 import PrevButton from './PrevButton';
 import * as Styled from './Buttons.styled';
+import { useSelector } from 'react-redux';
+import { selectStep } from '../../../features/registration/selectors';
+import { FIRST_STEP } from '../../../features/registration/constants';
 
 const Buttons = () => {
+  const step = useSelector(selectStep);
 
   return (
     <Styled.ButtonContainer>
-      <PrevButton />
+      {step !== FIRST_STEP && <PrevButton />}
       <NextButton />
     </Styled.ButtonContainer>
   );
--- src/components/registration/buttons/PrevButton.js
+++ src/components/registration/buttons/PrevButton.js
@@ -1,21 +1,14 @@
 import React, { useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Button } from 'rsuite';
-import { FIRST_STEP } from '../../../features/registration/constants';
 import { decreaseStep } from '../../../features/registration/registrationSlice';
-import { selectStep } from '../../../features/registration/selectors';
 
 const PrevButton = () => {
-  const step = useSelector(selectStep)
   const dispatch = useDispatch();
   const handleNextStep = useCallback(() => {
     dispatch(decreaseStep());
   }, [dispatch]);
 
-  if (step === FIRST_STEP) {
-    return null;
-  }
-  
   return (
     <Button type="button" onClick={handleNextStep}>
       prev
